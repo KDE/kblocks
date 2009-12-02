3022c0b4c004c9912b87366ab24f1cd243d02bae
Dapeng Zhang <zhang_beijing@yahoo.com>
2009-12-02 07:30:47 +0000

    license notes added
    svn path=/trunk/playground/games/kblocks-refactoring/; revision=1057372

--- KBlocksItemGroup.cpp
+++ KBlocksItemGroup.cpp
@@ -1,3 +1,13 @@
+/***************************************************************************
+*   KBlocks, a falling blocks game for KDE                                *
+*   Copyright (C) 2009 Zhongjie Cai <squall.leonhart.cai@gmail.com>       *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+***************************************************************************/
+
 #include "KBlocksItemGroup.h"
 
 #include <KDebug>
