37032b2697219943c5aa2abf1ebb72b7f4e33062
Montel Laurent <montel@kde.org>
2015-01-06 21:11:03 +0100

    Fix shortcut

--- KBlocksKeyboardPlayer.cpp
+++ KBlocksKeyboardPlayer.cpp
@@ -41,10 +41,7 @@ KBlocksKeyboardPlayer::KBlocksKeyboardPlayer(KXmlGuiWindow * parent, string name
 
 KBlocksKeyboardPlayer::~KBlocksKeyboardPlayer()
 {
-    if (mpKeyWindow)
-    {
-        delete mpKeyWindow;
-    }
+    delete mpKeyWindow;
 }
 
 void KBlocksKeyboardPlayer::startGame(SingleGameInterface * p)
@@ -86,37 +83,37 @@ void KBlocksKeyboardPlayer::bindKeys()
     rotatecw = mpKeyShortcuts->addAction( QLatin1String( "rotate_cw" ));
     rotatecw->setText(i18n("Rotate Piece Clockwise"));
     rotatecw->setIcon(QIcon::fromTheme( QLatin1String( "object-rotate-right" )));
-    rotatecw->setShortcuts( KShortcut( "z" ) );
+    mpKeyShortcuts->setDefaultShortcut(rotatecw, Qt::Key_Z);
     connect(rotatecw, SIGNAL(triggered(bool)), this, SLOT(rotateCW()));
     
     rotateccw = mpKeyShortcuts->addAction( QLatin1String( "rotate_ccw" ));
     rotateccw->setText(i18n("Rotate Piece Counter Clockwise"));
     rotateccw->setIcon(QIcon::fromTheme( QLatin1String( "object-rotate-left" )));
-    rotateccw->setShortcuts( KShortcut( Qt::Key_Up  ) );
+    mpKeyShortcuts->setDefaultShortcut(rotateccw, Qt::Key_Up );
     connect(rotateccw, SIGNAL(triggered(bool)), this, SLOT(rotateCCW()));
     
     moveleft = mpKeyShortcuts->addAction( QLatin1String( "move_left" ));
     moveleft->setText(i18n("Move Piece Left"));
     moveleft->setIcon(QIcon::fromTheme( QLatin1String( "arrow-left" )));
-    moveleft->setShortcuts( KShortcut( Qt::Key_Left ) );
+    mpKeyShortcuts->setDefaultShortcut(moveleft, Qt::Key_Left );
     connect(moveleft, SIGNAL(triggered(bool)), this, SLOT(moveLeft()));
     
     moveright = mpKeyShortcuts->addAction( QLatin1String( "move_right" ));
     moveright->setText(i18n("Move Piece Right"));
     moveright->setIcon(QIcon::fromTheme( QLatin1String( "arrow-right" )));
-    moveright->setShortcuts( KShortcut( Qt::Key_Right) );
+    mpKeyShortcuts->setDefaultShortcut(moveright, Qt::Key_Right);
     connect(moveright, SIGNAL(triggered(bool)), this, SLOT(moveRight()));
     
     movedown = mpKeyShortcuts->addAction( QLatin1String( "move_down" ));
     movedown->setText(i18n("Move Piece Down"));
     movedown->setIcon(QIcon::fromTheme( QLatin1String( "arrow-down" )));
-    movedown->setShortcuts( KShortcut( Qt::Key_Down ) );
+    mpKeyShortcuts->setDefaultShortcut(movedown, Qt::Key_Down );
     connect(movedown, SIGNAL(triggered(bool)), this, SLOT(moveDown()));
     
     pushdown = mpKeyShortcuts->addAction( QLatin1String( "push_down" ));
     pushdown->setText(i18n("Drop the Piece"));
     pushdown->setIcon(QIcon::fromTheme( QLatin1String( "arrow-down" )));
-    pushdown->setShortcuts( KShortcut( Qt::Key_Space ) );
+    mpKeyShortcuts->setDefaultShortcut(pushdown, Qt::Key_Space);
     connect(pushdown, SIGNAL(triggered(bool)), this, SLOT(pushDown()));
 }
 
