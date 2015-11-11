8e15ffc4e97ea9aa27dfab82d28abb0fe068aac9
Frederik Schwarzer <schwarzer@kde.org>
2015-11-11 17:01:22 +0100

    Add homepage link to about dialog.

--- main.cpp
+++ main.cpp
@@ -320,6 +320,7 @@ int main(int argc, char *argv[])
     aboutData.addAuthor(i18n("Dirk Leifeld"), i18n("Developer"), QStringLiteral("dirkleifeld@yahoo.de"));
     aboutData.addAuthor(i18n("Zhongjie Cai"), i18n("New design of KBlocks for AI and tetris research platform"), QStringLiteral("squall.leonhart.cai@gmail.com"));
     aboutData.addCredit(i18n("Johann Ollivier Lapeyre"), i18n("Oxygen art for KDE4"), QStringLiteral("johann.ollivierlapeyre@gmail.com"));
+    aboutData.setHomepage(QStringLiteral("https://www.kde.org/applications/games/kblocks/"));
 
     // Command line argument options
     QCommandLineParser parser;
