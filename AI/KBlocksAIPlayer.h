cbb2c0833abffb56dffa4adf5d8f65959fafa0ea
Julian Helfferich <julian.helfferich@googlemail.com>
2016-03-26 21:40:49 -0500

    Add email address to copyright line
    In 15 files, the copyright line did not contain an email address. This
    was flagged by krazy2. I have contacted the author and got his
    permission to add his email address to the copyright.

--- AI/KBlocksAIPlayer.h
+++ AI/KBlocksAIPlayer.h
@@ -1,11 +1,11 @@
 /***************************************************************************
-*   KBlocks, a falling blocks game for KDE                                *
-*   Copyright (C) 2010 University Freiburg                                *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
+*   KBlocks, a falling blocks game for KDE                                 *
+*   Copyright (C) 2010 University Freiburg <squall.leonhart.cai@gmail.com> *
+*                                                                          *
+*   This program is free software; you can redistribute it and/or modify   *
+*   it under the terms of the GNU General Public License as published by   *
+*   the Free Software Foundation; either version 2 of the License, or      *
+*   (at your option) any later version.                                    *
 ***************************************************************************/
 #ifndef KBLOCKSAIPLAYER_H
 #define KBLOCKSAIPLAYER_H
